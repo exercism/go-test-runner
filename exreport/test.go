9f547271889626dc194ce9307d2cfe73cfa8edec
Erik Schierboom <erik_schierboom@hotmail.com>
2020-01-02 14:13:31 +0100

    Remove obsolete fields

--- exreport/test.go
+++ exreport/test.go
@@ -2,9 +2,7 @@ package exreport
 
 // Test represents the result of a test
 type Test struct {
-	Name     string `json:"name"`
-	Status   string `json:"status"`
-	Expected string `json:"expected"`
-	Actual   string `json:"actual"`
-	Message  string `json:"message"`
+	Name    string `json:"name"`
+	Status  string `json:"status"`
+	Message string `json:"message"`
 }
