0dcd91c42a51a70741b768751c342fdce11d44ef
TehSphinX <tehsphinx@web.de>
2020-01-30 21:13:19 +0100

    change output of test-runner on compile error to adhere to the standard

--- exreport/report.go
+++ exreport/report.go
@@ -2,6 +2,7 @@ package exreport
 
 // Report represents a report of all tests of an exercise.
 type Report struct {
-	Status string `json:"status"`
-	Tests  []Test `json:"tests"`
+	Status  string `json:"status"`
+	Message string `json:"message,omitempty"`
+	Tests   []Test `json:"tests"`
 }
