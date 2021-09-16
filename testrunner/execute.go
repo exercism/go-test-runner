fffa91c65d08bfc136c63f3de97ef023377406e0
Jason Runkle <jmrunkle@google.com>
2021-09-15 22:59:39 -0700

    Use the `--short` option for `go test` (#38)
    Use the `--short` option for `go test`
    
    Co-authored-by: Eric Kingery <ekingery@users.noreply.github.com>

--- testrunner/execute.go
+++ testrunner/execute.go
@@ -181,7 +181,8 @@ func testCompile(input_dir string) bool {
 	return err == nil
 }
 
-// Run the "go test --json ." command, return output
+// Run the "go test --short --json ." command, return output
+// --short is used to exclude benchmark tests, given the spec / web UI currently cannot handle them
 func runTests(input_dir string) (bytes.Buffer, bool) {
 	goExe, err := exec.LookPath("go")
 	if err != nil {
@@ -192,7 +193,7 @@ func runTests(input_dir string) (bytes.Buffer, bool) {
 	testCmd := &exec.Cmd{
 		Dir:    input_dir,
 		Path:   goExe,
-		Args:   []string{goExe, "test", "--json", "."},
+		Args:   []string{goExe, "test", "--short", "--json", "."},
 		Stdout: &stdout,
 		Stderr: &stderr,
 	}
