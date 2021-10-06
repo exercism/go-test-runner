b6393ab75da7b6bd6d0526e55608da8009f6dc34
June <12543047+junedev@users.noreply.github.com>
2021-10-06 18:50:09 +0200

    Avoid random test order (#49)
    * extend passing test to include multiple test cases and subtests
    * use slice instead of map to collect test results

--- testrunner/testdata/practice/passing/passing_test.go
+++ testrunner/testdata/practice/passing/passing_test.go
@@ -5,10 +5,27 @@ import (
 	"testing"
 )
 
-// Trivial passing test example
-func TestTrivialPass(t *testing.T) {
+// Trivial passing test example 1
+func TestTrivialPass1(t *testing.T) {
+	t.Run("subtest 1.1", func(t *testing.T) {
+		if true != true {
+			t.Fatal("Should never happen!")
+		}
+		fmt.Println("sample passing subtest output 1.1")
+	})
+
+	t.Run("subtest 1.2", func(t *testing.T) {
+		if true != true {
+			t.Fatal("Should never happen!")
+		}
+		fmt.Println("sample passing subtest output 1.2")
+	})
+}
+
+// Trivial passing test example 2
+func TestTrivialPass2(t *testing.T) {
 	if true != true {
-		t.Fatal("This was supposed to be a tautological statement!")
+		t.Fatal("Should never happen!")
 	}
-	fmt.Println("sample passing test output")
+	fmt.Println("sample passing test output 2")
 }
