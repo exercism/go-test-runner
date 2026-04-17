2994936bb223bb5351e27da9154eaff3673f7585
Isaac Good <github@isaacgood.com>
2026-04-16 22:07:27 -0700

    Add a script to test example/exemplar solutions with the test runner tool

---
+++ bin/test-examples.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+
+# Check the script is called with a directory as $1
+(( $# )) || { echo "Usage: $0 <track-repo> [exercise-path ...]"; exit 1; }
+track_repo="${1%/}"
+[[ -d "${track_repo}" ]] || { echo "Usage: $0 <track-repo> [exercise-path ...]"; exit 1; }
+shift
+
+run_tests () {
+    if (( $# != 0 )); then
+        exercises="$@"
+    else
+        exercises=( "${track_repo}"/exercises/concept/*/ "${track_repo}"/exercises/practice/*/ )
+    fi
+    for exercise in "${exercises[@]}"; do
+        src="$(jq -r '.files|.example//.exemplar|.[0]' "$exercise/.meta/config.json")"
+        dst="$(jq -r '.files.solution[0]' "$exercise/.meta/config.json")"
+        cp "${exercise}/${src}" "${exercise}/${dst}"
+        if ! output=$(go run . "$exercise" "${PWD}" 2>&1) || [[ -n "${output}" ]]; then
+            printf 'Exercise %s does not test cleanly.\n%s\n' "${exercise}" "${output}"
+        fi
+        git -C "$exercise" checkout HEAD -- "${dst}"
+    done
+}
+
+run_tests "$@"
